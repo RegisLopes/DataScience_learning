dcbed1bfdbe16e472d5503c8a436c115f45372a8
Regis <>
2021-05-03 08:20:26 +0000

    viel

--- main.py
+++ main.py
@@ -1,39 +1,59 @@
-class Node:
-  def __init__(self, data, next):
-    self.data = data
-    self.next = next
+#this __ini__(self) is #called a method, a #constructor method, what #does it do actually?
 
-class LinkedList:  
+
+class Stack():
+  
   def __init__(self):
-    self.head = None
+    self.a = []
   
-  def add_at_front(self,data):
-    self.head = Node(data, self.head)
-
-  def add_at_end(self, data):
-    if not self.head:
-      self.head = Node(data, None)
-      return
-    curr = self.head
-    while curr.next:
-      curr = curr.next
-    curr.next = Node(data,None)
-
-  def get_last_noe(self):
-    n= self.head
-    while(n.next != None):
-      n = n.next
-    return n.data
+  def is_empty(self):
+    return self.a == []
   
-  def print_list(self):
-    n = self.head
-    while n!=None:
-      print(n.data, end="=>")
-      n = n.next
-    print()
+  def push_item(self,value):
+    self.a.append(value)
+  
+  def pop_item(self):
+    if self.is_empty():
+      print('Stack is empty')
+    return self.a.pop()
+  
+  def peek(self):
+    if self.is_empty():
+      print('Stack is empty')
+    return self.a[-1]
+  
+  def size(self):
+    return len(self.a)
+  
+  def display(self):
+    print(self.a)
+  
+  def balanced(self):
+    if self.is_empty:
+      print('The parentheses are balanced')
+    else:
+      print('The parentheses are not balanced')
 
 
-x=myList.head
-while x!=None:
-  print(x.data)
-  x=x.next
+
+
+x=Stack()
+a=list(input(''))()
+for i in a:
+  if i!="(" or i!=")":
+    
+print(a)
+print(type(a))
+
+
+#print(x.is_empty())
+#x.push_item(1)
+#x.push_item(2)
+#x.push_item(5)
+#x.push_item(8)
+#x.display()
+#x.pop_item()
+#x.display()
+#print(x.is_empty())
+
+#https://www.youtube.com/watch?v=z2Vs8iRMmEs
